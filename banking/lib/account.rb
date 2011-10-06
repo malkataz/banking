f8886fed2bd602edef1251e8f3a5b4d0df5cc66b
Malkataz M <malkataz@yahoo.gr>
2011-10-06 15:41:51 +0100

    starting_balance equals to 100

--- banking/lib/account.rb
+++ banking/lib/account.rb
@@ -1,7 +1,14 @@
 # account.rb
 
 class Account
+
+  def initialize(starting_balance = 0.0)
+    @starting_balance = starting_balance
+  end
+
   def balance
     0.00
+    return @starting_balance
   end
+
 end
--- banking/spec/account_spec.rb
+++ banking/spec/account_spec.rb
@@ -10,4 +10,9 @@ describe Account do
     account.balance.should == 0.0
   end
 
+   it "should have a balance equal to the starting balance" do
+     starting_balance = 100.00
+     subject = Account.new(starting_balance)
+     subject.balance.should == starting_balance
+   end
 end
